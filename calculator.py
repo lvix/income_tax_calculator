546d2006a414a9b91dcd813fbc631c91970a56c4
369696807 <369696807@qq.com>
2017-11-17 00:43:03 +0800

    improved calculation

--- calculator.py
+++ calculator.py
@@ -11,31 +11,24 @@ if __name__ == '__main__':
     except:
         print('Parameter Error')
         exit(1)
-    
+    if salary < 0:
+        print('Parameter Error')
+        exit(1)
     # constants
     tax_threshold = 3500
     insurance = 0
     tax_rate = [0.03, 0.10, 0.20, 0.25, 0.30, 0.35, 0.45]
     fixed_value = [0, 105, 555, 1005, 2755, 5505, 13505]
-
+    tax_range = [0, 1500, 4500, 9000, 35000, 55000, 80000]
     # figure out the amount that should be taxed
     tax_amount = salary - insurance - tax_threshold
-
-    if tax_amount <= 1500:
-        level = 0
-    elif 1500 <= tax_amount < 4500:
-        level = 1
-    elif 4500 <= tax_amount < 9000:
-        level = 2
-    elif 9000 <= tax_amount < 35000:
-        level = 3
-    elif 35000 <= tax_amount < 55000:
-        level = 4
-    elif 55000 <= tax_amount < 80000:
-        level = 5
-    elif 80000 <= tax_amount:
+    if tax_amount > tax_range[-1]:
         level = 6
-
+    else:
+        for level in range(7):
+            if tax_range[level] <= tax_amount < tax_range[level +1]:
+                break
+                
     # figure out the final tax amount 
     tax_result = tax_amount * tax_rate[level] - fixed_value[level]
 
